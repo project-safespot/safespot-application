57e8da8b3fa30b846840e3a0a5e263c20aa17e50
naladoodong <naladoodong@gmail.com>
2026-05-12 01:24:50 +0900

    feat: add batch occupancy repository method

--- services/async-worker/src/main/java/com/safespot/asyncworker/repository/EvacuationEntryRepository.java
+++ services/async-worker/src/main/java/com/safespot/asyncworker/repository/EvacuationEntryRepository.java
@@ -1,6 +1,11 @@
 package com.safespot.asyncworker.repository;
 
+import java.util.Collection;
+import java.util.Map;
+
 public interface EvacuationEntryRepository {
 
     int countEntered(Long shelterId);
+
+    Map<Long, Integer> countEnteredByShelterIds(Collection<Long> shelterIds);
 }
